7459e83f93e387a8a3e7aa1c031aada7ac819a80
Liane Hampe <liaham@xmera.de>
2022-04-27 20:24:29 +0200

    Adds internationalization to mapping_function

--- app/models/mapping_function.rb
+++ app/models/mapping_function.rb
@@ -19,6 +19,8 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 class MappingFunction < BaseFunction
+  include Redmine::I18n
+
   def calculate
     values
     map = custom_field.enumerations.where(x_index: values[0], y_index: values[1]).take
